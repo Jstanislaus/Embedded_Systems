a06ae731fca991a7c66a7ac683a686a3a3a4355b
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2024-04-17 18:53:15 +0100

    GPIO

--- Pi_stuff.py
+++ Pi_stuff.py
@@ -1,7 +1,11 @@
-import pifacedigitalio
+import RPi.GPIO as GPIO
 import time
-pf = pifacedigitalio.PiFaceDigital()
+# use P1 header pin numbering convention
+GPIO.setmode(GPIO.BOARD)
+
+# Set up the GPIO channels - one input and one output
+GPIO.setup(4, GPIO.IN)
 while True:
-    input_state = pf.input_pins[1].value
-    print(input_state)
+    input_value = GPIO.input(4)
+    print(input_value)
     time.sleep(3)
