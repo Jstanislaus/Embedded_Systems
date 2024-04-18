c72d8990251e73a03dd107e700fe076aafde2506
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2024-04-18 14:18:04 +0100

    .

--- Pi_stuff.py
+++ Pi_stuff.py
@@ -2,6 +2,55 @@ import pyaudio
 import wave
 import matplotlib.pyplot as plt
 
+def visualize(path: str): 
+    
+    # reading the audio file 
+    raw = wave.open(path) 
+      
+    # reads all the frames  
+    # -1 indicates all or max frames 
+    signal = raw.readframes(-1) 
+    signal = np.frombuffer(signal, dtype ="int16") 
+      
+    # gets the frame rate 
+    f_rate = raw.getframerate() 
+  
+    # to Plot the x-axis in seconds  
+    # you need get the frame rate  
+    # and divide by size of your signal 
+    # to create a Time Vector  
+    # spaced linearly with the size  
+    # of the audio file 
+    time = np.linspace( 
+        0, # start 
+        len(signal) / f_rate, 
+        num = len(signal) 
+    ) 
+  
+    # using matplotlib to plot 
+    # creates a new figure 
+    plt.figure(1) 
+      
+    # title of the plot 
+    plt.title("Sound Wave") 
+      
+    # label of x-axis 
+    plt.xlabel("Time") 
+     
+    # actual plotting 
+    plt.plot(time, signal) 
+      
+    # shows the plot  
+    # in new window 
+    plt.show() 
+  
+    # you can also save 
+    # the plot using 
+    # plt.savefig('filename') 
+
+
+
+
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
@@ -47,3 +96,4 @@ wavefile.close()
 plt.figure()
 plt.plot(ampl)
 plt.show()
+visualize(wav_output_filename)
